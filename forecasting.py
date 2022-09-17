157c281d04fbcde1a6c9a78cb341d7dc4c9fb9ae
Oscar Ferreira <osky.ferreira@gmail.com>
2022-09-16 21:34:38 -0300

    Oscar
    Forecating
    Regresion lineal

---
+++ forecasting.py
@@ -0,0 +1,414 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 21:07:13 2022
+
+@author: Usuario
+"""
+
+# Tratamiento de datos
+# ==============================================================================
+import numpy as np
+import pandas as pd
+
+# Gráficos
+# ==============================================================================
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
+plt.style.use('fivethirtyeight')
+
+# Modelado y Forecasting
+# ==============================================================================
+from sklearn.linear_model import Ridge
+from lightgbm import LGBMRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error
+from skforecast.ForecasterAutoreg import ForecasterAutoreg
+from skforecast.ForecasterAutoregMultiOutput import ForecasterAutoregMultiOutput
+from skforecast.model_selection import grid_search_forecaster
+from skforecast.model_selection import backtesting_forecaster
+
+# Configuración warnings
+# ==============================================================================
+import warnings
+warnings.filterwarnings('ignore')
+"""
+# Descarga de datos
+# ==============================================================================
+url = ('https://raw.githubusercontent.com/JoaquinAmatRodrigo/skforecast/master/' +
+       'data/vic_elec.csv')
+datos = pd.read_csv(url, sep=',')
+datos.info()
+
+# Conversión del formato fecha
+# ==============================================================================
+datos['Time'] = pd.to_datetime(datos['Time'], format='%Y-%m-%dT%H:%M:%SZ')
+datos = datos.set_index('Time')
+datos = datos.asfreq('30min')
+datos = datos.sort_index()
+
+# Verificar que un índice temporal está completo
+# ==============================================================================
+(datos.index == pd.date_range(start=datos.index.min(),
+                              end=datos.index.max(),
+                              freq=datos.index.freq)).all()
+
+
+# Agregado en intervalos de 1H
+# ==============================================================================
+# Se elimina la columna Date para que no genere error al agregar. La columna Holiday
+# no genera error ya que es booleana y se trata como 0-1.
+datos = datos.drop(columns='Date')
+datos = datos.resample(rule='H', closed='left', label ='right').mean()
+datos
+"""
+# Separación datos train-val-test
+# ==============================================================================
+datos = datos.loc['2012-01-01 00:00:00': '2014-12-30 23:00:00']
+fin_train = '2013-12-31 23:59:00'
+fin_validacion = '2014-11-30 23:59:00'
+datos_train = datos.loc[: fin_train, :]
+datos_val   = datos.loc[fin_train:fin_validacion, :]
+datos_test  = datos.loc[fin_validacion:, :]
+
+print(f"Fechas train      : {datos_train.index.min()} --- {datos_train.index.max()}  (n={len(datos_train)})")
+print(f"Fechas validacion : {datos_val.index.min()} --- {datos_val.index.max()}  (n={len(datos_val)})")
+print(f"Fechas test       : {datos_test.index.min()} --- {datos_test.index.max()}  (n={len(datos_test)})")
+
+
+
+# Gráfico serie temporal
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(12, 4))
+datos_train.Demand.plot(ax=ax, label='entrenamiento', linewidth=1)
+datos_val.Demand.plot(ax=ax, label='validación', linewidth=1)
+datos_test.Demand.plot(ax=ax, label='test', linewidth=1)
+ax.set_title('Demanda eléctrica')
+ax.legend();
+
+# Gráfico serie temporal con zoom
+# ==============================================================================
+zoom = ('2013-05-01 14:00:00','2013-06-01 14:00:00')
+
+fig = plt.figure(figsize=(12, 6))
+grid = plt.GridSpec(nrows=8, ncols=1, hspace=0.6, wspace=0)
+
+main_ax = fig.add_subplot(grid[1:3, :])
+zoom_ax = fig.add_subplot(grid[5:, :])
+
+datos.Demand.plot(ax=main_ax, c='black', alpha=0.5, linewidth=0.5)
+min_y = min(datos.Demand)
+max_y = max(datos.Demand)
+main_ax.fill_between(zoom, min_y, max_y, facecolor='blue', alpha=0.5, zorder=0)
+main_ax.set_xlabel('')
+
+datos.loc[zoom[0]: zoom[1]].Demand.plot(ax=zoom_ax, color='blue', linewidth=2)
+
+main_ax.set_title(f'Demanda electricidad: {datos.index.min()}, {datos.index.max()}', fontsize=14)
+zoom_ax.set_title(f'Demanda electricidad: {zoom}', fontsize=14)
+plt.subplots_adjust(hspace=1)
+
+# Gráfico boxplot para estacionalidad anual
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(7, 3.5))
+datos['mes'] = datos.index.month
+datos.boxplot(column='Demand', by='mes', ax=ax,)
+datos.groupby('mes')['Demand'].median().plot(style='o-', linewidth=0.8, ax=ax)
+ax.set_ylabel('Demand')
+ax.set_title('Distribución demanda por mes')
+fig.suptitle('');
+
+# Gráfico boxplot para estacionalidad semanal
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(7, 3.5))
+datos['dia_semana'] = datos.index.day_of_week + 1
+datos.boxplot(column='Demand', by='dia_semana', ax=ax)
+datos.groupby('dia_semana')['Demand'].median().plot(style='o-', linewidth=0.8, ax=ax)
+ax.set_ylabel('Demand')
+ax.set_title('Distribución demanda por día de la semana')
+fig.suptitle('');
+
+
+# Gráfico boxplot para estacionalidad diaria
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(9, 3.5))
+datos['hora_dia'] = datos.index.hour + 1
+datos.boxplot(column='Demand', by='hora_dia', ax=ax)
+datos.groupby('hora_dia')['Demand'].median().plot(style='o-', linewidth=0.8, ax=ax)
+ax.set_ylabel('Demand')
+ax.set_title('Distribución demanda por hora del día')
+fig.suptitle('');
+
+# Grafico violinplot
+# ==============================================================================
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7, 3.5))
+sns.violinplot(
+    x       = 'Demand',
+    y       = 'Holiday',
+    data    = datos.assign(Holiday = datos.Holiday.astype(str)),
+    palette = 'tab10',
+    ax      = ax
+)
+ax.set_title('Distribución de la demanda entre festivos y no festivos')
+ax.set_xlabel('demanda')
+ax.set_ylabel('festivo');
+
+# Gráfico autocorrelación
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(7, 3))
+plot_acf(datos.Demand, ax=ax, lags=60)
+plt.show()
+
+# Gráfico autocorrelación parcial
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(7, 3))
+plot_pacf(datos.Demand, ax=ax, lags=60)
+plt.show()
+
+ Crear y entrenar forecaster
+# ==============================================================================
+forecaster = ForecasterAutoreg(
+                regressor = make_pipeline(StandardScaler(), Ridge()),
+                lags      = 24
+             )
+
+forecaster.fit(y=datos.loc[:fin_validacion, 'Demand'])
+forecaster
+
+# Backtest
+# ==============================================================================
+metrica, predicciones = backtesting_forecaster(
+                            forecaster = forecaster,
+                            y          = datos.Demand,
+                            initial_train_size = len(datos.loc[:fin_validacion]),
+                            fixed_train_size   = False,
+                            steps      = 24,
+                            metric     = 'mean_absolute_error',
+                            refit      = False,
+                            verbose    = True
+                        )
+
+# Gráfico
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(12, 3.5))
+datos.loc[predicciones.index, 'Demand'].plot(ax=ax, linewidth=2, label='test')
+predicciones.plot(linewidth=2, label='predicción', ax=ax)
+ax.set_title('Predicción vs demanda real')
+ax.legend();
+
+# Error backtest
+# ==============================================================================
+print(f'Error backtest: {metrica}')
+
+# Grid search de hiperparámetros
+# ==============================================================================
+forecaster = ForecasterAutoreg(
+                    regressor = make_pipeline(StandardScaler(), Ridge()),
+                    lags      = 24 # Este valor será remplazado en el grid search
+             )
+
+# Lags utilizados como predictores
+lags_grid = [5, 24, [1, 2, 3, 23, 24, 25, 47, 48, 49]]
+
+# Hiperparámetros del regresor
+param_grid = {'ridge__alpha': np.logspace(-3, 5, 10)}
+
+resultados_grid = grid_search_forecaster(
+                        forecaster  = forecaster,
+                        y           = datos.loc[:fin_validacion, 'Demand'],
+                        param_grid  = param_grid,
+                        lags_grid   = lags_grid,
+                        steps       = 24,
+                        metric      = 'mean_absolute_error',
+                        refit       = False,
+                        initial_train_size = len(datos[:fin_train]),
+                        fixed_train_size   = False,
+                        return_best = True,
+                        verbose     = False
+                  )
+
+# Resultados Grid Search
+# ==============================================================================
+resultados_grid
+
+forecaster
+
+# Backtest modelo final
+# ==============================================================================
+metrica, predicciones = backtesting_forecaster(
+                            forecaster = forecaster,
+                            y          = datos.Demand,
+                            initial_train_size = len(datos[:fin_validacion]),
+                            fixed_train_size   = False,
+                            steps      = 24,
+                            metric     = 'mean_absolute_error',
+                            refit      = False,
+                            verbose    = False
+                        )
+
+fig, ax = plt.subplots(figsize=(12, 3.5))
+datos.loc[predicciones.index, 'Demand'].plot(linewidth=2, label='test', ax=ax)
+predicciones.plot(linewidth=2, label='predicción', ax=ax)
+ax.set_title('Predicción vs demanda real')
+ax.legend();
+
+# Error backtest
+# ==============================================================================
+print(f'Error backtest: {metrica}')
+
+# Importancia predictores
+# ==============================================================================
+forecaster.get_feature_importance()
+
+# Se convierte la columna Holiday de boolean a integer
+datos.loc[:,'Holiday'] = datos['Holiday'].astype(int)
+datos_train.loc[:,'Holiday'] = datos_train['Holiday'].astype(int)
+datos_test.loc[:,'Holiday'] = datos_test['Holiday'].astype(int)
+# Crear y entrenar forecaster
+# ==============================================================================
+forecaster = ForecasterAutoreg(
+                regressor = make_pipeline(StandardScaler(), Ridge(alpha=215.44)),
+                lags      = [1, 2, 3, 23, 24, 25, 47, 48, 49],
+             )
+
+forecaster.fit(y=datos.Demand[:fin_validacion], exog=datos.Holiday[:fin_validacion])
+forecaster
+
+# Backtest
+# ==============================================================================
+predicciones = backtest_predict_next_24h(
+                    forecaster = forecaster,
+                    y          = datos.loc[fin_validacion:, 'Demand'],
+                    exog       = datos.loc[fin_validacion:, 'Holiday'],
+                    hour_init_prediction = 11,
+                    verbose    = False
+               )
+
+# Gráfico
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(12, 3.5))
+datos.loc[predicciones.index, 'Demand'].plot(linewidth=2, label='test', ax=ax)
+predicciones.plot(linewidth=2, label='predicción', ax=ax)
+ax.set_title('Predicción vs demanda real')
+ax.legend();
+
+# Error backtest
+# ==============================================================================
+error = mean_absolute_error(
+            y_true = datos.loc[predicciones.index, 'Demand'],
+            y_pred = predicciones
+        )
+
+print(f"Error de backtest: {error}")
+
+ Creación de nuevas variables exógenas
+# ==============================================================================
+# Al ser datos  horarios y empezar a las 00:00 se tienen que desplazar los valores
+# de 24 en 24.
+datos.loc[:,'Holiday_day_before'] = datos['Holiday'].shift(24)
+datos.loc[:,'Holiday_next_day']   = datos['Holiday'].shift(-24)
+datos=datos.dropna()
+
+# One hot encoding del día de la semana y la hora del día
+datos=pd.get_dummies(datos, columns=['dia_semana', 'hora_dia'])
+datos.head(3)
+
+# Crear y entrenar forecaster
+# ==============================================================================
+forecaster = ForecasterAutoreg(
+                regressor = make_pipeline(StandardScaler(), Ridge(alpha=215.44)),
+                lags      = [1, 2, 3, 23, 24, 25, 47, 48, 49],
+             )
+
+exog = [column for column in datos.columns if column.startswith(('dia', 'hora', 'Holiday'))]
+forecaster.fit(y=datos.Demand[: fin_validacion], exog=datos[exog][: fin_validacion])
+
+# Backtest - Predicción diaria anticipada
+# ==============================================================================
+predicciones = backtest_predict_next_24h(
+                    forecaster = forecaster,
+                    y          = datos.loc[fin_validacion:, 'Demand'],
+                    exog       = datos.loc[fin_validacion:, exog],
+                    hour_init_prediction = 11,
+                    verbose    = False
+               )
+# Gráfico
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(12, 3.5))
+datos.loc[predicciones.index, 'Demand'].plot(linewidth=2, label='test', ax=ax)
+predicciones.plot(linewidth=2, label='predicción', ax=ax)
+ax.set_title('Predicción vs demanda real')
+ax.legend();
+
+# Error backtest
+# ==============================================================================
+error = mean_absolute_error(
+            y_true = datos.loc[predicciones.index, 'Demand'],
+            y_pred = predicciones
+        )
+
+print(f"Error de backtest: {error}")
+
+
+# Grid search de hiperparámetros - Entrenamiento y tuning del Forecaster
+# ==============================================================================
+forecaster = ForecasterAutoregMultiOutput(
+                regressor = LGBMRegressor(max_depth=4),
+                steps     = 36,
+                lags      = 24 # Este valor será remplazado en el grid search
+             )
+
+# Lags utilizados como predictores
+lags_grid = [[1, 2, 3, 23, 24], [1, 2, 3, 23, 24, 25, 47, 48, 49]]
+
+# Hiperparámetros del regresor
+param_grid = {'n_estimators': [100, 500],
+              'max_depth': [4, 6]}
+
+resultados_grid = grid_search_forecaster(
+                        forecaster         = forecaster,
+                        y                  = datos.loc[:fin_validacion, 'Demand'],
+                        exog               = datos.loc[:fin_validacion, exog],
+                        param_grid         = param_grid,
+                        lags_grid          = lags_grid,
+                        steps              = 36,
+                        metric             = 'mean_absolute_error',
+                        refit              = False,
+                        initial_train_size = len(datos.loc[:fin_train]),
+                        return_best        = True,
+                        verbose            = False
+                  )
+
+
+# Backtest - Prediccion diaria anticipada
+# ==============================================================================
+predicciones = backtest_predict_next_24h(
+                    forecaster = forecaster,
+                    y          = datos.loc[fin_validacion:, 'Demand'],
+                    exog       = datos.loc[fin_validacion:, exog],
+                    hour_init_prediction = 11,
+                    verbose    = False
+               )
+
+# Gráfico
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(12, 3.5))
+datos.loc[predicciones.index, 'Demand'].plot(linewidth=2, label='test', ax=ax)
+predicciones.plot(linewidth=2, label='predicción', ax=ax)
+ax.set_title('Predicción vs demanda real')
+ax.legend();
+
+# Error backtest
+# ==============================================================================
+error = mean_absolute_error(
+            y_true = datos.loc[predicciones.index, 'Demand'],
+            y_pred = predicciones
+        )
+
+print(f"Error de backtest: {error}")
+
+import session_info
+session_info.show(html=False)
