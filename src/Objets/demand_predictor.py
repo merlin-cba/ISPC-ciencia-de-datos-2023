e666dc6df52416bb02b4df129b42fb6f45d2702a
Oscar Luis FERREIRA <58222080+merlin-cba@users.noreply.github.com>
2023-07-04 17:59:47 -0300

    Create demand_predictor.py

---
+++ src/Objets/demand_predictor.py
@@ -0,0 +1,21 @@
+import pickle
+
+class DemandPredictor:
+    def __init__(self, model_file):
+        self.model_file = model_file
+        self.linear_model = None
+        self.neural_model = None
+    
+    def load_models(self):
+        with open(self.model_file, 'rb') as f:
+            self.linear_model, self.neural_model = pickle.load(f)
+    
+    def predict(self, features):
+        linear_predictions = self.linear_model.predict(features)
+        neural_predictions = self.neural_model.predict(features)
+        return linear_predictions, neural_predictions
+    
+    def calculate_accuracy(self, actual_values, predicted_values):
+        mse = mean_squared_error(actual_values, predicted_values)
+        r2 = r2_score(actual_values, predicted_values)
+        return mse, r2
