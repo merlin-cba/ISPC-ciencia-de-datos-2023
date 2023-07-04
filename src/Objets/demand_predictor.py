e1144250104874dd0956ae0a99dd39d687b753b3
Oscar Luis FERREIRA <58222080+merlin-cba@users.noreply.github.com>
2023-07-04 18:00:57 -0300

    Update demand_predictor.py

--- src/Objets/demand_predictor.py
+++ src/Objets/demand_predictor.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 import pickle
 
 class DemandPredictor:
